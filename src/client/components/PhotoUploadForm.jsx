4bf7bf3a77fb7443284f83bd6e1576de390d170c
mbasagoitia <marika.basagoitia@gmail.com>
2025-01-24 11:13:08 -0500

    fixed typo in path to server uploads folder

--- src/client/components/PhotoUploadForm.jsx
+++ src/client/components/PhotoUploadForm.jsx
@@ -3,29 +3,40 @@ import { Form, Button, Container } from "react-bootstrap";
 import uploadPhotos from "../helpers/uploadPhotos";
 
 const UploadForm = () => {
+
+  const photoUploadData = {
+    directory: "",
+    images: []
+  }
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [directory, setDirectory] = useState("");
 
-  const handleFileChange = (event) => setSelectedFiles(Array.from(event.target.files));
+  const handleFileChange = (event) => {
+    setSelectedFiles(Array.from(event.target.files));
+  } 
+
   const handleDirectoryChange = (event) => setDirectory(event.target.value);
 
   const handleUpload = async (event) => {
     event.preventDefault();
 
-    const formData = new FormData();
-    selectedFiles.forEach((file, index) => {
-      formData.append(`images`, file);
-    });
-    formData.append("directory", directory);
+    if (directory && selectedFiles.length > 0) {
 
-    uploadPhotos(formData);
+      selectedFiles.forEach((file, index) => {
+        photoUploadData.images = [...photoUploadData.images, file]
+      });
+      
+      photoUploadData.directory = directory;
+      console.log(photoUploadData);
+      uploadPhotos(photoUploadData);
+    }
   };
 
   return (
     
     <Form onSubmit={handleUpload}>
         <Form.Group controlId="attendance">
-          <Form.Label>Select Directory</Form.Label>
+          <Form.Label>Directory</Form.Label>
           <Form.Control
             as="select"
             name="directory"
@@ -33,6 +44,7 @@ const UploadForm = () => {
             onChange={handleDirectoryChange}
             required
           >
+            <option value="" selected disabled hidden>Choose Directory...</option>
             <option value="ceremony">Ceremony</option>
             <option value="reception">Reception</option>
             <option value="other">Other</option>
--- src/client/helpers/uploadPhotos.js
+++ src/client/helpers/uploadPhotos.js
@@ -1,24 +1,27 @@
-const uploadPhotos = async (formData) => {
-    try {
-        const response = await fetch('http://localhost:5000/photos/upload', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ formData })
-        });
-          if (response.ok) {
-            // const { path } = await response.json();
-            console.log("Photos uploaded successfully");
-            // Redirect or alert as needed, fetch directory
-          } else {
-            console.error('Upload failed');
-          }
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
-};
+const uploadPhotos = async (photoUploadData) => {
+  try {
+    const formData = new FormData();
 
+    photoUploadData.images.forEach((image) => {
+      formData.append("images", image);
+    });
+
+    formData.append("directory", photoUploadData.directory);
+
+    const response = await fetch("http://localhost:5000/photos/upload", {
+      method: "POST",
+      credentials: "include",
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log("Photos uploaded successfully");
+    } else {
+      console.error("Upload failed");
+    }
+  } catch (error) {
+    console.error("Error uploading file:", error);
+  }
+};
 
 export default uploadPhotos;
