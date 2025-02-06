51d5e20e2d877581ec995d6800bdc6cfe93922ed
mbasagoitia <marika.basagoitia@gmail.com>
2025-02-05 21:58:22 -0500

    corrected google auth link

--- src/client/pages/Photos.jsx
+++ src/client/pages/Photos.jsx
@@ -2,6 +2,9 @@ import { motion } from "framer-motion";
 import PreviewCollection from "../components/PreviewCollection";
 
 const Photos = ({ user }) => {
+
+    const googleAuthLink = "https://accounts.google.com/o/oauth2/v2/auth?client_id=879762652639-0va910evds26g88gpals8q1v210keln1.apps.googleusercontent.com&redirect_uri=https://wedding.basagoitia.net/api/auth/google/callback&response_type=code&scope=email%20profile";
+
     return (
         <div className="photos">
             <motion.h1
@@ -21,7 +24,7 @@ const Photos = ({ user }) => {
                         Please sign in with your Google account to upload your photos.
                     </p>
                     <div className="mt-4">
-                        <a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=879762652639-0va910evds26g88gpals8q1v210keln1.apps.googleusercontent.com&redirect_uri=https://wedding.basagoitia.net/api/auth/google/callback&response_type=code&scope=email%20profile"><button className="btn-primary">Link Google Account</button></a>
+                        <a href={googleAuthLink}><button className="btn-primary">Link Google Account</button></a>
                     </div>
                 </motion.div>
             ) : null}
--- src/client/pages/UploadPhotos.jsx
+++ src/client/pages/UploadPhotos.jsx
@@ -4,6 +4,8 @@ import PreviewCollection from "../components/PreviewCollection.jsx";
 
 const UploadPhotos = ({ user }) => {
 
+    const googleAuthLink = "https://accounts.google.com/o/oauth2/v2/auth?client_id=879762652639-0va910evds26g88gpals8q1v210keln1.apps.googleusercontent.com&redirect_uri=https://wedding.basagoitia.net/api/auth/google/callback&response_type=code&scope=email%20profile";
+
     return (
         <div className="photo-upload">
             <div className="pu-upload">
@@ -12,7 +14,7 @@ const UploadPhotos = ({ user }) => {
                 <div>
                     <p className="text-center">You must be logged in to upload photos.</p>
                     <div className="d-flex justify-content-center">
-                        <a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=879762652639-0va910evds26g88gpals8q1v210keln1.apps.googleusercontent.com&redirect_uri=https://wedding.basagoitia.net/auth/google/callback&response_type=code&scope=email%20profile"><button className="btn-primary">Link Google Account</button></a>
+                        <a href={googleAuthLink}><button className="btn-primary">Link Google Account</button></a>
                     </div>
                 </div>
                 )}
